351e4bfffdc1ea85de7c786cc6f2a09e2772bf1c
Frank Schmidt <frank.schmidt@codecentric.de>
2021-12-03 14:22:32 +0100

    format: nimpretty

--- day1.nim
+++ day1.nim
@@ -44,7 +44,7 @@ when isMainModule:
       let input = @[1, 1, 1, 2, 2, 0]
       check(num_increases_sliding_window(input) == 2)
 
-  let input = readFile("day1.txt").strip() # remove final newline
+  let input = readFile("day1.txt").strip()       # remove final newline
   let inputLines = input.splitLines().map(parseInt)
   echo format("part 1: $#", num_increases(inputLines))
   echo "part 2: {num_increases_sliding_window(inputLines)}".fmt
